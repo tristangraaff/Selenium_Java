d65a5fd0f27c29d5867204c2b1d1b4ef1d4b29a3
Tristan Graaff <t.graaff@squerist.nl>
2022-02-03 11:53:43 +0100

    contuing the script

--- src/java_introduction/Locators.java
+++ src/java_introduction/Locators.java
@@ -1,5 +1,7 @@
 package java_introduction;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,10 +13,18 @@ public class Locators {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://mail.yahoo.com");
 		System.out.println(driver.getTitle());
+		driver.findElement(By.cssSelector("input[id='login-username']")).sendKeys("barryprisoner@yahoo.com");
+		driver.findElement(By.cssSelector("input[id='login-username']")).clear();
+		driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("tristangraaff");
+		driver.findElement(By.xpath("//input[@name=\"username\"]")).clear();
 		driver.findElement(By.id("login-username")).sendKeys("tristangraaff");
 		driver.findElement(By.id("login-signin")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		driver.findElement(By.id("login-passwd")).sendKeys("whyisntitworking");
 		driver.findElement(By.id("login-signin")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error-msg")).getText());
+//		driver.findElement(By.linkText("Dummy text")).click();
+		
 	}
 
 }
